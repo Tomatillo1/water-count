d0a29572737fd2b8d63953c69f0742f43b0e72d5
Tomatillo1 <totofarkas@gmail.com>
2024-02-28 11:09:59 +0100

    end design

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 let start = new Date(2024, 0, 1);
 const startNumber = 3255728347.20;
 const startnumber2 = 13865964;
-const changeNumber = document.querySelector('.number');
-const changeNumber2 = document.querySelector('.number2');
+const changeNumber = document.querySelector('.quantity1');
+const changeNumber2 = document.querySelector('.quantity2');
 setInterval(changeIntoNewNumber, 1000)
 
 function changeIntoNewNumber() {
@@ -10,6 +10,6 @@ function changeIntoNewNumber() {
     let secondsSinceStart = Math.floor((now - start) / 1000);
     let newValue = secondsSinceStart*21.24+startNumber;
     let newValue2 = secondsSinceStart*0.09+startnumber2;
-    changeNumber.innerHTML = newValue.toFixed(2)
-    changeNumber2.innerHTML = newValue2.toFixed(2)
+    changeNumber.innerHTML = newValue.toLocaleString("da-DK");
+    changeNumber2.innerHTML = newValue2.toLocaleString("da-DK");
 }
